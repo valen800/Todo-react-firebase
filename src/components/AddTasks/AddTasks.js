d32f9f0a9f8b828c693226b08f96cdd86eed20db
valen800 <varulo.cvcv@gmail.com>
2020-04-28 12:22:27 +0200

    AddTask to firebase

--- src/components/AddTasks/AddTasks.js
+++ src/components/AddTasks/AddTasks.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import {} from "lodash";
+import { isEmpty } from "lodash";
 import { ReactComponent as Send } from "../../assets/send.svg";
 
+import firebase from "../../utils/firebase";
+import "firebase/firestore";
+
 import "./AddTasks.scss";
+
+const db = firebase.firestore(firebase);
 
 export default function AddTasks() {
   const [task, setTask] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault(); //No recarga la pagina
-    console.log(task);
+    if (!isEmpty(task)) {
+      console.log("No esta vacio");
+      db.collection("tasks")
+        .add({
+          name: task,
+          completed: false,
+        })
+        .then(() => {
+          console.log("Tarea creada");
+        })
+        .catch(() => {
+          console.log("Error");
+        });
+    }
   };
 
   return (
